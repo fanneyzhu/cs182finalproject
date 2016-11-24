a560d0375bd6e927618e1fd0b6b4acc83d42c9ee
Fanney Zhu <fanneyzhu@college.harvard.edu>
2016-11-23 22:33:44 -0500

    add domain

--- packing.py
+++ packing.py
@@ -3,62 +3,72 @@ import sys
 import timeit
 
 class Box:
-	def __init__(self, length, width):
+	def __init__(self, label, length, width):
+		self.label = label
 		self.length = length
 		self.width = width
 		self.area = length*width
 
+
 class BinPacking:
 	def __init__(self, storage_dimensions, boxes):
-		# (length * width)
+		# length and width of storage space
 		self.l = storage_dimensions[0]
 		self.w = storage_dimensions[1]
-		self.storage = []
+		
+		self.storage_matrix = []
 		for i in xrange(self.l):
-			self.storage.append([])
+			self.storage_matrix.append([])
 			for j in xrange(self.w):
-				self.storage[i].append(-1)
+				self.storage_matrix[i].append(-1)
+		
+		# [[(0,topleftx0,toplefty0),(0,bottomrightx0, bottomrighty0)],
+		#  [(1,topleftx1,toplefty1),(1,bottomrightx1, bottomrighty1)], ...]
+		self.storage_list = []
+		for i in xrange(self.l):
+			self.storage_list.append([])
 		
 		# [Box1, Box2, Box3, ...]
 		self.boxes = boxes
+
+		# domain for box
+		self.domain = {}
 
 	# greedy approach - sort boxes from largest to smallest area
 	# and pack the largest ones first
 	def greedyAlgorithm(self):
 		packed = 0
-		areas = [(i, j) for i,j in enumerate(self.boxes)]
-		areas = sorted(areas, key=lambda x:(-x[1].area))
-
-		for label, box in areas:
+		boxes = sorted(self.boxes, key=lambda x:(-x.area));
+		for box in boxes:
 			orientation, row, column = self.findEmptySpot(box.length, box.width)
 			if orientation == -1:
-				print "Cannot find empty spot for box", label
+				print "Cannot find empty spot for box", box.label
 			elif orientation == 0:
 				for i in xrange(row, row+box.length):
 					for j in xrange(column, column+box.width):
-						self.storage[i][j] = label
+						self.storage_matrix[i][j] = box.label
 				packed += 1
 			elif orientation == 1:
 				for i in xrange(row, row+box.width):
 					for j in xrange(column, column+box.length):
-						self.storage[i][j] = label
+						self.storage_matrix[i][j] = box.label
 				packed += 1
 
-		print "Packed ", packed, "out of", len(self.boxes), "boxes"
+		print "Packed ", packed, "out of", len(boxes), "boxes"
 		self.prettyPrintStorage()
 	
-	# returns first empty spot found for a specified length and width and 
-	# orientation to place it in: 0 for length*width, 1 for width*length
-	# if none is found, returns -1
+	# returns first empty spot (top left corner of box) found for a specified 
+	# length and width and orientation to place it in: 0 for length*width, 1 
+	# for width*length. if none is found, returns -1
 	def findEmptySpot(self, length, width):
 		for row in xrange(self.l):
 			for column in xrange(self.w):
-				if self.storage[row][column] == -1:
+				if self.storage_matrix[row][column] == -1:
 					# check if there is space for length*width
 					if row+length < self.l and column+width < self.w:
 						for i in xrange(row, row+length):
 							for j in xrange(column, column+width):
-								if (self.storage[i][j] != -1):
+								if (self.storage_matrix[i][j] != -1):
 									continue
 								else:
 									if i == row+length-1 and j == column+width-1:
@@ -67,7 +77,7 @@ class BinPacking:
 					elif row+width < self.l and column+length < self.w:
 						for i in xrange(row, row+width):
 							for j in xrange(column, column+length):
-								if (self.storage[i][j] != -1):
+								if (self.storage_matrix[i][j] != -1):
 									continue
 								else:
 									if i == row+width-1 and j == column+length-1:
@@ -75,18 +85,86 @@ class BinPacking:
 
 		return (-1,-1, -1)
 
+	# creates a new version of the storage list with box assigned
+	def setVariable(self, label, topleftx, toplefty, bottomrightx, bottomrighty):
+		box = [(label, topleftx, toplefty), (label, bottomrightx, bottomrighty)]
+		newStorageList = deepcopy(self.storage_list)
+		num_stored = len(newStorageList)
+		for i in xrange(num_stored):
+			if label == newStorageList[i][0][0]:
+				newStorageList[i] = box
+			elif label != newStorageList[i][0][0] and i == num_stored-1:
+				newStorageList.append(box)
+				break
+		return newStorageList
+
+	# returns true if assignment is complete
+	def complete(self):
+		if len(self.storage_list) == len(self.boxes):
+			return True
+		return False
+
+	# returns a list of the available locations for a given box 
+	# in the storage space
+	def getDomain(self, input_box):
+		domain = [] 
+		num_stored = len(self.storage_list)
+		for row in xrange(self.l): 
+			for column in xrange(self.w):
+				if row+input_box.width < self.l and column+input_box.width < self.w:
+					for i in xrange(num_stored): 
+						result = box_overlap(row, column, row+input_box.width, 
+							column+input_box.length, self.storage_list[i][0][1], 
+							self.storage_list[i][0][2], self.storage_list[i][1][1],
+							self.storage_list[i][1][2])
+						if result == True:
+							continue
+						elif result == False and i == num_stored-1:
+							domain.append([(row,column),(row+width,column+length)])
+				elif row+input_box.length < self.l and column+input_box.width < self.w:
+					for i in xrange(num_stored): 
+						result = box_overlap(row, column, row+input_box.length, 
+							column+input_box.width, self.storage_list[i][0][1], 
+							self.storage_list[i][0][2], self.storage_list[i][1][1],
+							self.storage_list[i][1][2])
+						if result == True:
+							continue
+						elif result == False and i == num_stored-1:
+							domain.append([(row,column),(row+width,column+length)])
+
+		return domain 
+
+	# helper function that checks if two boxes are overlapping 
+	def box_overlap(l1x, l1y, r1x, r1y, l2x, l2y, r2x, r2y):
+		# check if one rectangle is on left side of other
+		if l1x > r2x or l2x > r1x: 
+			return False
+		# check if one rectangle is above other
+		if l1y < r2y or l2y < r1y:
+			return False
+		return True
+
+	# returns next variable to try assigning
+	def nextVariable(self):
+		pass
+
+	# returns new assignments with each possible value assigned to the variable
+	# returned by 'nextVariable'
+	def getSuccessors(self):
+		pass
+
 	# pretty prints the storage space. where the box is located in the storage
 	# space is indicated by its box number/label. -1 indicates an empty space
 	def prettyPrintStorage(self):
 		for i in xrange(self.l):
 			for j in xrange(self.w):
-				print self.storage[i][j],
+				print self.storage_matrix[i][j],
 			print
 
 
 def main():
 	storage_dimensions = (10,10)
-	boxes = [Box(2,2), Box(1,3), Box(4,4), Box(2,2), Box(2,4), Box(2,5), Box(3,3), Box(3,5), Box(4,4)]
+	boxes = [Box(0,2,2), Box(1,1,3), Box(2,4,4), Box(3,2,2), Box(4,2,4), Box(5,2,5), Box(6,3,3), Box(7,3,5), Box(8,4,4)]
 	start = BinPacking(storage_dimensions, boxes)
 	start.greedyAlgorithm()
 
